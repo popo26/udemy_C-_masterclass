0ddbc899615840e7baecb9817fd54018077bcab0
popo26 <mynameispopo26@gmail.com>
2022-12-01 14:41:53 +1300

    complete OOP

---
+++ 78_OOP_Properties/78_OOP_Properties/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _78_OOP_Properties
+{
+    class Box
+    {
+        //member variable
+        private int volume;
+
+        //property - getter and setter1
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        //property -getter and setter2
+        public int FrontSurface
+        {
+            get
+            {
+                return Length * Height;
+            }
+        
+        }
+
+        //constractor
+        public Box(int length, int height, int width)
+        {
+            Length = length;
+            Height= height;
+            Width = width;  
+        }
+
+        public int GetVolume()
+        { 
+            this.volume = Length * Height * Width;
+            return volume;
+        }
+    }
+}
---
+++ 78_OOP_Properties/78_OOP_Properties/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _78_OOP_Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(1,2,3);
+
+            Console.WriteLine("This box's front surface is " + box.FrontSurface);
+            Console.WriteLine("This box's volume is {0}", box.GetVolume());
+        }
+    }
+}
