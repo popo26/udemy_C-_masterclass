40c4c4ebd5fce4199bdfd010bdb4902ce3228dbf
popo26 <mynameispopo26@gmail.com>
2022-12-03 09:52:25 +1300

    stack

---
+++ 101_Hashtables/101_Hashtables/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Write a program that will interate through each element of the students array and insert them into a hashtable.
+ * If a student with the same ID already exists in the hashtable skip it and display the following error:
+ * "Sorry, A student with the same ID already exists".
+ * Hint: Usee the method ContainsKey(0 to check weather a student with the same ID already exists.
+ */
+
+using System;
+using System.Collections;
+
+namespace _101_Hashtables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable studentTable = new Hashtable();
+
+            Student[] students = new Student[5];
+            students[0] = new Student(1, "Denis", 88);
+            students[1] = new Student(2, "Olaf", 97);
+            students[2] = new Student(6, "Ranger", 65);
+            students[3] = new Student(1, "Luise", 73);
+            students[4] = new Student(4, "Levi", 58);
+
+            foreach (Student s in students)
+            {
+                if (!studentTable.Contains(s.Id))
+                {
+                    studentTable.Add(s.Id, s);
+                    Console.WriteLine("Student ID {0} was added.", s.Id);
+                }
+                else 
+                {
+                    Console.WriteLine("Student ID already exits. ID:{0}", s.Id);
+                }
+            }
+        }
+    }
+
+    class Student
+    { 
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public float GPA { get; set; }
+
+        public Student(int id, string name, float GPA)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.GPA = GPA;
+        }
+        
+    }
+    
+}
---
+++ 105_stack _and_queues/105_stack _and_queues/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _105_stack__and_queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            Console.WriteLine("Top value in the stack is {0}", stack.Peek());
+            stack.Push(2);
+            Console.WriteLine("Top value in the stack is {0}", stack.Peek());
+            stack.Push(3);
+            Console.WriteLine("Top value in the stack is {0}", stack.Peek());
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine("The top value {0} was removed from the stack", stack.Pop());
+                Console.WriteLine("Current stack count is {0}", stack.Count);
+            }
+
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Stack<int> myStack = new Stack<int>();
+
+
+            Console.WriteLine("the numbers in the arrays are: ");
+            
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+                myStack.Push(num);
+                
+            }
+
+            //numbers in reverse
+            Console.WriteLine("");
+            Console.WriteLine("The numbers in reverse : ");
+            while (myStack.Count > 0)
+            {
+                int num = myStack.Pop();
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
