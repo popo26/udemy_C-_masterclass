9b5efffb10f172d3dc67682765f6562aef61d86e
popo26 <mynameispopo26@gmail.com>
2022-12-14 13:16:22 +1300

    datetime

---
+++ 146_DateTime/146_DateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _146_DateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("{0} o'clock {1} minutes and {2} seconds", now.Hour, now.Minute, now.Second);
+
+            DateTime dateTime = new DateTime();
+            Console.WriteLine("Write your birthday in this format: yyyy-mm-dd");
+            string input = Console.ReadLine();
+            if (DateTime.TryParse(input, out dateTime))
+            {
+                Console.WriteLine(dateTime);
+                TimeSpan daysPassed = now.Subtract(dateTime);
+                Console.WriteLine("Days passed since : {0}", daysPassed.Days);
+            }
+        }
+    }
+}
